0da748b1f9bdd83a57ed41a8d8e0541f09847d3b
yongge666 <2550702985@qq.com>
2016-02-22 23:35:50 +0800

    直播项目构建

---
+++ application/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee
+ * Date: 2016/2/18
+ * Time: 10:40
+ */
+header('location:home');
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee
+ * Date: 2016/2/18
+ * Time: 10:51
+ */
+header('location:application/home');
